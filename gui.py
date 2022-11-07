6cfaa1d2c873b99fb6033222ee8cb81a92938e7c
Diksha Magotra <dikshamagotra2021@gmail.com>
2022-11-06 22:20:01 -0500

    Added state for rating database

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ import pandas as pd
 from utilities.db_functions import import_mongodb_to_dataframe,download_database,import_csv_to_dataframe
 from utilities.design_functions import add_bg_from_url,hide_menu_style,side_bar_menu,progress_bar,color_survived,info_text
 from utilities.df_functions import check_col_datatypes,st_show_datatypes,st_show_nullvalues, filter_tv_rows,st_show_head
-from utilities.state_functions import clean_col,null_val,filter_tv
+from utilities.state_functions import clean_col,null_val,filter_tv,rating_val,null_tv
 
 
 
@@ -16,27 +16,27 @@ add_bg_from_url()
 
 def main():
     st.title("Anime Recommendation System")
-    st.button(
-        label="Import Data from Kaggle",
-        on_click = download_database
-        )
-    
+    st.button(label="Import Anime Data from Kaggle", on_click = download_database)
+
     options = side_bar_menu()
+
     if options == "Data Cleaning":
         data_frame = import_mongodb_to_dataframe(collection_name="anime")
         rating_df = import_csv_to_dataframe() 
 
-
         if not data_frame.empty:
-
+            st.write(data_frame.head(10))
             if 'clean_columns' not in st.session_state:
                 st.session_state.clean_columns = False
 
             if 'check_null_val' not in st.session_state:
                 st.session_state.check_null_val = False
 
-            if 'filter_tv' not in st.session_state:
-                st.session_state.filter_tv = False
+            if 'filter_tv_val' not in st.session_state:
+                st.session_state.filter_tv_val = False
+
+            if 'null_tv_val' not in st.session_state:
+                st.session_state.null_tv_val = False
 
             st.subheader("Data Cleaning")
             col1,col2 = st.columns(2)
@@ -45,7 +45,7 @@ def main():
             st.button('Clean columns datatypes', on_click=clean_col)
             if st.session_state.clean_columns:
                 progress_bar()
-                anime_df = check_col_datatypes(dataFrame = anime_df)
+                anime_df = check_col_datatypes(dataFrame = data_frame)
                 df_types = pd.DataFrame(anime_df.dtypes, columns=['Data Type'])
                 
                 with col2:
@@ -54,22 +54,30 @@ def main():
                     
             st.button('Check Null Values', on_click=null_val)
             if st.session_state.check_null_val:
+                progress_bar()
                 st_show_nullvalues(dataFrame=anime_df)
+            
             st.button('Filter | TV', on_click=filter_tv)
-            if st.session_state.filter_tv:
-                filter_tv_rows(dataFrame=anime_df)
-            # if st.button('Null Values Check'):
-            #     anime_df = check_col_datatypes(dataFrame = anime_df) #TODO: Diksha 
-            #     tv_df = filter_tv_rows(dataFrame=anime_df) #TODO: Diksha 
-            #     st_show_nullvalues(dataFrame=tv_df)  
-            #     st.caption(info_text)       
+            if st.session_state.filter_tv_val:
+                progress_bar()
+                tv_df = filter_tv_rows(dataFrame=anime_df)
+                st_show_head(tv_df)
+
+            st.button('Null Values Check | TV', on_click=null_tv)
+            if st.session_state.null_tv_val:
+                progress_bar()
+                tv_df = filter_tv_rows(dataFrame=anime_df) #TODO: Diksha 
+                st_show_nullvalues(dataFrame=tv_df)  
+                st.caption(info_text)         
         else:
             st.warning("MongoDB Anime Empty!")
-        
-        if not rating_df.empty:
+        if 'rating_data' not in st.session_state:
+            st.session_state.rating_data = False
+        st.button("Import Ratings Data from Kaggle", on_click = rating_val)
+        if st.session_state.rating_data:
             st.write(rating_df.head(10))
         else:
-            st.warning("MongoDB Ratings Empty!")
+            st.warning("MongoDB Anime Empty!")
                 
     elif options == "Content-Based Recommendation":
         st.subheader("Content-Based Recommendation")
--- utilities/state_functions.py
+++ utilities/state_functions.py
@@ -2,7 +2,16 @@ import streamlit as st
 
 def clean_col():
     st.session_state.clean_columns = True
+
 def null_val():
     st.session_state.check_null_val = True
+
 def filter_tv():
-    st.session_state.filter_tv = True
+    st.session_state.filter_tv_val = True
+
+def rating_val():
+    st.session_state.rating_data = True
+
+def null_tv():
+    st.session_state.null_tv_val = True
+    
